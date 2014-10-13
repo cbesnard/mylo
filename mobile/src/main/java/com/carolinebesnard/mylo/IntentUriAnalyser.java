8176f076df47d4e15d775b8a1d73c2c623ec8c5c
Caroline Besnard <besnard.caro@gmail.com>
2014-10-13 22:55:08 +0200

    Finished to analyse every mylo shared url, TODO= maps shared url with goo.gl and cid

--- mobile/src/main/java/com/carolinebesnard/mylo/IntentUriAnalyser.java
+++ mobile/src/main/java/com/carolinebesnard/mylo/IntentUriAnalyser.java
@@ -2,6 +2,7 @@ package com.carolinebesnard.mylo;
 
 import android.content.Context;
 import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.util.Base64;
 import android.util.Log;
@@ -14,12 +15,15 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 /**
  * Created by carolinebesnard on 13/10/2014.
@@ -52,8 +56,8 @@ public class IntentUriAnalyser {
                     Log.i(TAG, "lat: "+myloPlace.getLatitude()+" & lon: "+myloPlace.getLongitude());
                     String url = urlBuilderFromMyloPlace(myloPlace);
                     return url;
-                }else{
-                    MyloPlace addr = Helper.getMyloPlaceFromQ(value);
+                }else{//PUBLIC PLACES
+                    MyloPlace addr = getMyloPlaceFromQ(value);
                     //GET url from MyloPlace
                     String url = urlBuilderFromMyloPlace(addr);
                     return url;
@@ -66,8 +70,7 @@ public class IntentUriAnalyser {
                     //GET url from MyloPlace
                     String url = urlBuilderFromMyloPlace(myloPlace);
                     return url;
-                }else{
-                    // UNKNOWN URL FORMAT
+                }else{// UNKNOWN URL FORMAT
                     return null;
                 }
             }
@@ -144,7 +147,58 @@ public class IntentUriAnalyser {
     }
 
     private static MyloPlace getMyloPlaceFromCid(String cid){
-        //TODO: ASK PLACES API WITH CID
+        Log.i(TAG,"in getMyloPlaceFromCid");
+        //TODO: FIND HOW TO ANALYSE CID
+        try{
+            //HTTP REQUEST TO GET LONG URL
+            /*String URL = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+cid+"&key=AIzaSyB60Ax0BPa8GaJFTSpJ3QuIkL3KP7IqviM";
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response = httpclient.execute(new HttpGet(URL));
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                out.close();
+                String responseString = out.toString();
+                Log.i(TAG,"responseString= "+responseString);
+            }*/
+        }catch (Exception e){
+
+        }
         return null;
+    }
+
+    public static MyloPlace getMyloPlaceFromQ(String address){
+        //GET ADDR FROM LOCATION
+        try {
+            String URL_base = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+            String URL = URL_base+URLEncoder.encode(address,"UTF-8")+"&key=AIzaSyB60Ax0BPa8GaJFTSpJ3QuIkL3KP7IqviM";
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response = httpclient.execute(new HttpGet(URL));
+            StatusLine statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                out.close();
+                String responseString = out.toString();
+                //Log.i(TAG,"responseString= "+responseString);
+                //JSON STUFF
+                JSONObject obj = new JSONObject(responseString);
+                JSONArray results = obj.getJSONArray("results");
+                JSONObject myresult = results.getJSONObject(0);
+                MyloPlace myloPlace = new MyloPlace(Locale.getDefault());
+                myloPlace.setAddress(myresult.getString("formatted_address"));
+                myloPlace.setFeatureName(myresult.getString("name"));
+                JSONObject location = myresult.getJSONObject("geometry").getJSONObject("location");
+                myloPlace.setLatitude(location.getDouble("lat"));
+                myloPlace.setLongitude(location.getDouble("lng"));
+                return myloPlace;
+            }else{return null;}
+        } catch (Exception e) {
+            e.printStackTrace();
+            //error case
+            Log.e(TAG, "Get location address ERROR: Couldn't get Address object, send error message to wear service");
+            return null;
+        }
     }
 }
