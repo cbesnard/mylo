c0be4bf5ee350841a8a7e4cb22cb2c481bc65290
Kevin <kevinj@theodo.fr>
2017-07-11 21:44:24 +0200

    Remove redux-firebase

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { geolocationReducer } from 'modules/geolocation'
 import { favoritesReducer } from 'modules/favorites'
-import { firebaseStateReducer } from 'react-redux-firebase'
 
 const appReducer = combineReducers({
-  firebase: firebaseStateReducer,
   geolocation: geolocationReducer,
   favorites: favoritesReducer,
 });
