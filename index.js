d2a0ea5080411e155ded7fb9ca7cc45836fc0085
Kevin <kevinj@theodo.fr>
2017-08-11 18:47:30 +0200

    redirect http to https

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    res.redirect('https://http://mylo-app.herokuapp.com'+req.url)
+  else
+    next()
+})
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
