60f3ca0883425f668f235c27734ed3ef895b96c6
Kevin <kevinj@theodo.fr>
2017-09-05 21:22:07 +0200

    Add API Map key

--- src/components/map/Map.component.js
+++ src/components/map/Map.component.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
-const googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry";
+const googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyDQHAzd9koui1ArKcX1lbFfEIxQNe1EkwE";
 
 const GettingStartedGoogleMap = withGoogleMap(props => {
   const { geolocation } = props;
